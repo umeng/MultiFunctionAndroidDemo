5b8d373f2b8a1f456975b2cfa52ad8c151caaf58
yajie.fyj <yajie.fyj@umeng.com>
2018-01-31 17:44:13 +0800

    update push ui

--- app/src/main/java/com/umeng/soexample/push/UpushActivity.java
+++ app/src/main/java/com/umeng/soexample/push/UpushActivity.java
@@ -1,24 +1,43 @@
 package com.umeng.soexample.push;
 
 import android.os.Bundle;
+import android.view.View;
 import com.umeng.soexample.BaseActivity;
 import com.umeng.soexample.R;
 
-/**
- * Created by wangfei on 2018/1/23.
- */
-
-public class UpushActivity extends BaseActivity {
+public class UpushActivity extends BaseActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setTitle("推送");
         setBackVisibily();
+
+        initClickListener();
+    }
+
+    private void initClickListener() {
+        findViewById(R.id.btn_add_tag).setOnClickListener(this);
     }
 
     @Override
     public int getLayout() {
         return R.layout.activity_upush;
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btn_add_tag:
+                new PushDialogFragment().show(getFragmentManager(), "haha");
+            case R.id.btn_delete_tag:
+            case R.id.btn_show_tags:
+            case R.id.btn_add_weighted_tag:
+            case R.id.btn_delete_weighted_tag:
+            case R.id.btn_show_weighted_tags:
+            case R.id.btn_add_alias:
+            case R.id.btn_delete_alias:
+
+        }
     }
 }
