9a9fbd2bb6271f84f61661f1986df1f79e6dd7f1
千顷 <yj131525@umeng.com>
2018-11-21 20:29:00 +0800

    更新分享SDK v6.9.4

---
+++ app/src/main/java/com/umeng/soexample/twitterapi/TwitterShareReceiver.java
@@ -0,0 +1,11 @@
+package com.umeng.soexample.twitterapi;
+
+import com.umeng.socialize.handler.UMTwitterReceiver;
+
+/**
+ * Created by umeng on 2018/11/13.
+ */
+
+public class TwitterShareReceiver extends UMTwitterReceiver {
+
+}
