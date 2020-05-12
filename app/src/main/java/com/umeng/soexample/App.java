6a8a50594b12d9695bf532022c214c67fba7fee2
yujie <yj131525@umeng.com>
2020-05-12 10:23:25 +0800

    1.分享SDK更新至版本v7.0.2。

--- app/src/main/java/com/umeng/soexample/App.java
+++ app/src/main/java/com/umeng/soexample/App.java
@@ -230,6 +230,7 @@ public class App extends Application {
         PlatformConfig.setSinaWeibo("3921700954", "04b48b094faeb16683c32669824ebdad", "http://sns.whalecloud.com");
         PlatformConfig.setYixin("yxc0614e80c9304c11b0391514d09f13bf");
         PlatformConfig.setQQZone("100424468", "c7394704798a158208a74ab60104f0ba");
+        PlatformConfig.setQQFileProvider("com.tencent.sample2.fileprovider");
         PlatformConfig.setTwitter("3aIN7fuF685MuZ7jtXkQxalyi", "MK6FEYG63eWcpDFgRYw4w9puJhzDl0tyuqWjZ3M7XJuuG7mMbO");
         PlatformConfig.setAlipay("2015111700822536");
         PlatformConfig.setLaiwang("laiwangd497e70d4", "d497e70d4c3e4efeab1381476bac4c5e");
