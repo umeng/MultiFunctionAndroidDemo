7255a028aae9cdc7eea94eb6c63b63bd82eacb0c
yujie <yj131525@umeng.com>
2020-09-26 15:54:40 +0800

    1.分享SDK升级到v7.1.2。2.Demo工程targetSdkVersion升级到30。3.QQ和微信支持FileProvider方式分享带图消息。

--- app/src/main/java/com/umeng/soexample/App.java
+++ app/src/main/java/com/umeng/soexample/App.java
@@ -228,10 +228,12 @@ public class App extends Application {
 
     {
         PlatformConfig.setWeixin("wxdc1e388c3822c80b", "3baf1193c85774b3fd9d18447d76cab0");
+        PlatformConfig.setWXFileProvider("com.tencent.sample2.fileprovider");
         //豆瓣RENREN平台目前只能在服务器端配置
         PlatformConfig.setSinaWeibo("3921700954", "04b48b094faeb16683c32669824ebdad", "http://sns.whalecloud.com");
         PlatformConfig.setYixin("yxc0614e80c9304c11b0391514d09f13bf");
         PlatformConfig.setQQZone("101830139", "5d63ae8858f1caab67715ccd6c18d7a5");
+        PlatformConfig.setQQFileProvider("com.tencent.sample2.fileprovider");
         PlatformConfig.setTwitter("3aIN7fuF685MuZ7jtXkQxalyi", "MK6FEYG63eWcpDFgRYw4w9puJhzDl0tyuqWjZ3M7XJuuG7mMbO");
         PlatformConfig.setAlipay("2015111700822536");
         PlatformConfig.setLaiwang("laiwangd497e70d4", "d497e70d4c3e4efeab1381476bac4c5e");
